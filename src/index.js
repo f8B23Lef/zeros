09d4c6c4b116718dc46e24206feee2e4bac53eb5
f8B23Lef <mal7rish@yandex.ru>
2018-09-30 17:18:34 +0300

    zeros/rewrite to ES6 notation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 module.exports = function getZerosCount(number) {
 
-  var zeros = 0;
+  let zeros = 0;
   
-  for (var i = 5; i <= number; i *= 5) {
+  for (let i = 5; i <= number; i *= 5) {
     zeros += Math.trunc(number / i); 
   }
     
